6477679461042b246a50b1c256092f36ad2f6db1
HYOBIN <52150400+lottt105@users.noreply.github.com>
2023-06-14 19:25:33 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,13 +1,12 @@
+const gcd = (num1, num2) => {
+    const r = num1 % num2;
+    if(r === 0) return num2;
+    return gcd(num2, r);
+} 
+
 const solution = (numer1, denom1, numer2, denom2) => {
-    const gcd = (num1, num2) => {
-        const r = num1 % num2;
-        if(r === 0) return num2;
-        return gcd(num2, r);
-    }
-    
     let numer = (numer1 * denom2) + (numer2 * denom1);
     let denom = denom1 * denom2;
     const remainder = gcd(numer, denom);
-    
     return [numer / remainder, denom / remainder];
 }
