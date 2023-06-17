749dd63b82bc7f7abde8cb3728f2d687a3dbbdfa
HYOBIN <52150400+lottt105@users.noreply.github.com>
2023-06-17 18:54:50 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 43.61 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', (line) => {
+    input = line.split(' ');
+}).on('close', () => {
+    for (let i = 1; i <= Number(input[0]); i++){
+        console.log("*".repeat(i));
+    }
+});
