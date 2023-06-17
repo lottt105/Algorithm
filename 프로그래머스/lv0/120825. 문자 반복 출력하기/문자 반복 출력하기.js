962953030747d9371f360dd6814a5f0641979f81
HYOBIN <52150400+lottt105@users.noreply.github.com>
2023-06-17 19:56:35 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,6 +1,1 @@
-const solution = (my_string, n) => {
-    let str = "";
-    for (let s of my_string) str += s.repeat(n);
-    return str;
-    
-}
+const solution = (my_string, n) => [...my_string].map(s => s.repeat(n)).join("");
