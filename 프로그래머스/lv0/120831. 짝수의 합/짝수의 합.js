ef548f3443256a0e7c09be274f9619b49768a703
HYOBIN <52150400+lottt105@users.noreply.github.com>
2023-06-18 21:27:16 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,4 +1,1 @@
-const solution = (n) => {
-    if (n == 1) return 0;
-    return n % 2 ? (2 + n - 1) * ((n - 1) / 4) : (2 + n) * (n / 4);
-}
+const solution = (n) => n % 2 ? (2 + n - 1) * ((n - 1) / 4) : (2 + n) * (n / 4);
