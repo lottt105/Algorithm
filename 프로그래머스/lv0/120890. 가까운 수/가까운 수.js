3d508f4a16f267c5c3884560de4bcb1a5ef6fa96
HYOBIN <52150400+lottt105@users.noreply.github.com>
2023-07-04 17:17:02 +0900

    [level 0] Title: 가까운 수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.js
@@ -1,10 +1,5 @@
 const solution = (array, n) => {
     array.sort((a, b) => a - b);
-    let min_num = 101
-    for (let i of array) {
-        if (Math.abs(i - n) < Math.abs(min_num - n)) {
-            min_num = i;
-        }
-    }
-    return min_num;
+    let abs_arr = array.map(a => Math.abs(a - n))
+    return array[abs_arr.indexOf(Math.min(...abs_arr))];
 }
