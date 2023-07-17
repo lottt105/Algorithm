c0ad5a894d261645d83e7f0fb07d810088f2a2df
HYOBIN <52150400+lottt105@users.noreply.github.com>
2023-07-17 23:27:56 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -0,0 +1,10 @@
+function solution(common) {
+    let result = 0;
+    if(common[1] - common[0] === common[2] - common[1]) {
+        result = common.pop() + (common[1] - common[0]);
+    }
+    else if(common[1] / common[0] === common[2] / common[1]) {
+        result = common.pop() * (common[1] / common[0]);
+    }
+    return result;
+}
