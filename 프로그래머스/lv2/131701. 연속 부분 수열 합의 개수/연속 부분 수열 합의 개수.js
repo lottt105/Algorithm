1abbe83d22140b743e9292e4666976dd740b846f
HYOBIN <52150400+lottt105@users.noreply.github.com>
2023-09-02 00:23:56 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 756.78 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,12 +1,12 @@
 function solution(elements) {
     const len = elements.length;
     elements = [...elements, ...elements];
-    const result = [];
+    const result = new Set();
     for(let i = 1; i < len; i++){
         for(let j = 0; j < len; j++){
-            result.push(elements.slice(j, j+i).reduce((a, c) => a += c));
+            result.add(elements.slice(j, j+i).reduce((a, c) => a += c));
         }
     }
-    result.push(elements.reduce((a, c) => a += c));
-    return new Set(result).size;
+    result.add(elements.reduce((a, c) => a += c));
+    return result.size;
 }
