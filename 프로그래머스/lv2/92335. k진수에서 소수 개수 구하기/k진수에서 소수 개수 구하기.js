6ca74038aa2fcd508ec3a4e719fbdc093e607142
HYOBIN <52150400+lottt105@users.noreply.github.com>
2023-09-09 20:44:39 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -1,11 +1,7 @@
 const isPrime = (num) => {
-    if(!num || num === '1'){
-        return false;
-    }
-    for(let i = 2; i <= Math.floor(Math.sqrt(num)); i++){
-        if(num % i === 0) {
-            return false;
-        }
+    if(!num || num === '1') return false;
+    for(let i = 2; i <= Math.sqrt(num); i++){
+        if(num % i === 0) return false;
     }
     return true;
 }
