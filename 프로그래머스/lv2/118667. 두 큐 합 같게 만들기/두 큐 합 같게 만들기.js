a5f59940ff9895fffe57f1de41c5e8d0a9dd1442
HYOBIN <52150400+lottt105@users.noreply.github.com>
2023-09-16 11:43:44 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 15.15 ms, Memory: 44.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -0,0 +1,26 @@
+function solution(queue1, queue2) {
+    const len = queue1.length * 3;
+    let result = 0;
+    let sum1 = queue1.reduce((a, c) => a + c, 0);
+    let sum2 = queue2.reduce((a, c) => a + c, 0);
+    let idx1 = 0, idx2 = 0;
+    if((sum1 + sum2) % 2 !== 0) return -1;
+    
+    for(let i = 0; i < len; i++){
+        if(sum1 === sum2) return result;
+        else if(sum1 < sum2){
+            const n = queue2[idx2++];
+            queue1.push(n);
+            sum1 += n;
+            sum2 -= n;
+        } else {
+            const n = queue1[idx1++];
+            queue2.push(n);
+            sum1 -= n;
+            sum2 += n;
+        }
+        result++;
+        if(queue1.length === 0 || queue2.length === 0) break;
+    }
+    return -1;
+}
