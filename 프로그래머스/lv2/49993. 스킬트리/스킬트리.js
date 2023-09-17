cf4a29c040ee210c7e6696ac7b800fc7c4c78d8d
HYOBIN <52150400+lottt105@users.noreply.github.com>
2023-09-17 16:48:50 +0900

    [level 2] Title: 스킬트리, Time: 0.13 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.js
@@ -1,24 +1,24 @@
-// function solution(skill, skill_trees) {
-//     let result = 0;
-//     const reg = new RegExp(`[${skill}]`, 'g');
-//     for(let i = 0; i < skill_trees.length; i++){
-//         const s = skill_trees[i];
-//         const skills = s.match(reg).join('');
-//         if(skill.startsWith(skills)) result++;
-//     }
-//     return result;
-// }
 function solution(skill, skill_trees) {
-
-    let answer = 0;
-    const skillList = skill.split("");
-
-    for (let skills of skill_trees) {
-        let temp = [];
-        for (let s of skills) {
-            if (skillList.includes(s)) temp.push(s);
-        }
-        if (skill.startsWith(temp.join(""))) answer += 1;
+    let result = 0;
+    const reg = new RegExp(`[${skill}]`, 'g');
+    for(let skills of skill_trees){
+        let s = skills.match(reg)
+        s ? s = s.join('') : result++;
+        if(skill.startsWith(s)) result++;
     }
-    return answer;
+    return result;
 }
+// function solution(skill, skill_trees) {
+
+//     let answer = 0;
+//     const skillList = skill.split("");
+
+//     for (let skills of skill_trees) {
+//         let temp = [];
+//         for (let s of skills) {
+//             if (skillList.includes(s)) temp.push(s);
+//         }
+//         if (skill.startsWith(temp.join(""))) answer += 1;
+//     }
+//     return answer;
+// }
