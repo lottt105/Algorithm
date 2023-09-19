7d47ce235b8b6332164cf788d4d849cc95203dea
HYOBIN <52150400+lottt105@users.noreply.github.com>
2023-09-19 22:07:18 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 9.74 ms, Memory: 40.7 MB -BaekjoonHub

--- 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/README.md
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 쿼드압축 후 개수 세기 - 68936 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68936#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68936) 
 
 ### 성능 요약
 
-메모리: 40.7 MB, 시간: 12.46 ms
+메모리: 40.7 MB, 시간: 9.74 ms
 
 ### 구분
 
--- 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -2,22 +2,28 @@ function solution(arr) {
     let zero = 0;
     let one = 0;
     function quad(i1, j1, i2, j2){
+        // 요소의 개수가 1일때 zero / one에 +1
         if(i2-i1 === 1 && j2-j1 === 1){
             arr[i1][j1] === 1 ? one++ : zero++;
             return arr[i1][j1];
         }
+        // 각 box의 왼쪽위 꼭짓점, 오른쪽 아래 꼭짓점 
         const b1 = quad(i1, j1, (i1+i2)/2, (j1+j2)/2);
         const b2 = quad(i1, (j1+j2)/2, (i1+i2)/2, j2);
         const b3 = quad((i1+i2)/2, j1, i2, (j1+j2)/2);
         const b4 = quad((i1+i2)/2, (j1+j2)/2, i2, j2);
         const total = b1 + b2 + b3 + b4;
+        // b1이 0 또는 1 또는 4로 나누어 떨어지는 수(모두 1일 경우)이고,
         if(b1 < 2 || b1 % 4 === 0){
+            // b1 === b2 === b3 === b4 이면 zero / one에 -3
             if(b1 === b2 && b1 === b3 && b1 === b4){
                 total === 0 ? zero -= 3 : one -= 3;
+                // 모두 더한 값 return
                 return total;
             }
         }
-        return +`${b1}+${b2}+${b3}+${b4}`;
+        // 의미 없는 값 return
+        //return +`${b1}+${b2}+${b3}+${b4}`;
     }
     quad(0, 0, arr.length, arr.length)
     return [zero, one]
