b3f83677ebae9daa4e6be6f29f19de7f5bf84374
HYOBIN <52150400+lottt105@users.noreply.github.com>
2024-01-11 00:07:50 +0900

    [level 2] Title: 호텔 대실, Time: 14.29 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
@@ -0,0 +1,27 @@
+function solution(book_time) {
+    let temp = [];
+    const time_num = book_time.map(([start, end]) => {
+        temp = [...start.split(":"), ...end.split(":")];
+        return [(+temp[0]*60)+(+temp[1]), (+temp[2]*60)+(+temp[3])]
+    })
+    time_num.sort((a, b) => a[0] - b[0]);
+
+    const rooms = [time_num[0][1]+10];
+    
+    for(let t = 1; t < time_num.length; t++){
+        const [start, end] = time_num[t];
+        rooms.sort((a, b) => a - b);
+        
+        if(start < rooms[0]){
+            rooms.push(end + 10);
+            continue;
+        }
+        for(let i = 0; i < rooms.length; i++){
+            if(start >= rooms[i]){
+                rooms[i] = end + 10;
+                break;
+            }
+        }
+    }
+    return rooms.length;
+}
